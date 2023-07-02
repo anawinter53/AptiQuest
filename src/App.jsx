bf77ad5ea8ab40bbd34940db8374e87b7dc84f20
anawinter53 <anatolia.winter@yahoo.com>
2023-07-02 09:57:49 +0100

    add modal styling

--- src/App.jsx
+++ src/App.jsx
@@ -55,30 +55,6 @@ export default function App() {
   let i = 0;
   let score = 0;
 
-
-  // window.addEventListener('keydown', function(event) {
-  //   const key = event.key; 
-  //   console.log(score)
-  //   if(key == 'ArrowLeft' | key == 'ArrowRight' & completed == false) {
-  //     (key == arrowTests[i].correct_keycode) ? score ++ : score + 0;
-  //     console.log(score)
-  //     console.log(key)
-  //     console.log(completed)
-  //     if( i <= 6 ) {
-  //       i ++;
-  //       console.log(i, 'this is i')
-  //       $('.test-img').attr('src', arrowTests[i].img_link)
-  //     } else {
-  //       console.log(completed, 'I AM IN ELSE')
-  //       setCompleted(true)
-  //       console.log(completed)
-  //     }
-  //   }
-  //   if (completed == true) {
-  //     $('.popup').attr('open', true)
-  //   } 
-  // });
-
   useKeypress(['ArrowLeft', 'ArrowRight'], (event) => {
     if(event.key == 'ArrowLeft' | event.key == 'ArrowRight' & completed == false & i <= 6) {
       (event.key == arrowTests[i].correct_keycode) ? score ++ : score + 0;
@@ -91,22 +67,12 @@ export default function App() {
       console.log(completed)
       setCompleted(true)
       console.log(completed)
+      console.log(score/8 * 100)
+      localStorage.setItem('percentage', (score/8 * 100))
     }
-    if (completed == true) {
-      $('.popup').attr('open', true)
-    } 
   });
 
-  // useEffect(() => {
-  //   console.log(completed)
-  //   if (completed == true) {
-  //     console.log("the modal should be open")
-  //     $('.popup').attr('open', true)
-  //   }
-  // }, [completed])
-
-  
-
+  let percentage = parseInt(localStorage.getItem('percentage'))
 
   return (
     <div>
@@ -115,7 +81,7 @@ export default function App() {
           <img className='test-img' src={arrowTests[i].img_link} />
       </div>
       <RxCross2 className="icon" size="40px" color="#5441E1" style={{position: 'absolute', top: "40px", right: "20px"}}/>
-      <Popup className='popup' open={completed} modal><div> Modal content </div></Popup>
+      <Popup className='popup' open={completed} modal><div> Congrats! You got {percentage}% </div></Popup>
     </div>
   )
 }
