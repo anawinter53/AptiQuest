f19dbebba9014f30cf952d8229bf489857eb0d1a
anawinter53 <anatolia.winter@yahoo.com>
2023-07-02 04:54:51 +0100

    add jquery and more functions to progress to next test screen on keyboard click

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './App.css'
 import { AiFillInfoCircle } from "react-icons/ai";
 import { RxCross2 } from "react-icons/rx";
@@ -8,58 +9,65 @@ export default function App() {
     {
       id: 1,
       img_link: 'src/assets/arrow-test-1.png',
-      correct_keycode: '39'
+      correct_response: 'ArrowRight'
     },
     {
       id: 2,
       img_link: 'src/assets/arrow-test-2.png',
-      correct_keycode: '37'
+      correct_keycode: 'ArrowLeft'
     },
     {
       id: 3,
       img_link: 'src/assets/arrow-test-3.png',
-      correct_keycode: '39'
+      correct_keycode: 'ArrowRight'
     },
     {
       id: 4,
       img_link: 'src/assets/arrow-test-4.png',
-      correct_keycode: '37'
+      correct_keycode: 'ArrowLeft'
     },
     {
       id: 5,
       img_link: 'src/assets/arrow-test-5.png',
-      correct_keycode: '39'
+      correct_keycode: 'ArrowRight'
     },
     {
       id: 6,
       img_link: 'src/assets/arrow-test-6.png',
-      correct_keycode: '37'
+      correct_keycode: 'ArrowLeft'
     },
     {
       id: 7,
       img_link: 'src/assets/arrow-test-7.png',
-      correct_keycode: '39'
+      correct_keycode: 'ArrowRight'
     },
     {
       id: 8,
       img_link: 'src/assets/arrow-test-8.png',
-      correct_keycode: '39'
+      correct_keycode: 'ArrowRight'
     }
   ]
 
-  const i = 0
+  let i = 0
 
   window.addEventListener('keydown', function(event) {
     const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
     console.log(key)
+    i ++;
+    // this.location.reload()
+    $('.test-img').attr('src', arrowTests[i].img_link)
 });
+
+  useEffect(() => {
+    console.log(i)
+  }, [i])
 
 
   return (
     <div>
       <AiFillInfoCircle className="icon" size="40px" color="#5441E1" />
       <div className='test-space'>
-          <img src={arrowTests[i].img_link} />
+          <img className='test-img' src={arrowTests[i].img_link} />
       </div>
       <RxCross2 className="icon" size="40px" color="#5441E1" style={{position: 'absolute', top: "40px", right: "20px"}}/>
     </div>
